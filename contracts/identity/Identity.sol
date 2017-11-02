c3942f95c7577357663138a7317435301ba76c84
Shabtai Dvir <shay.mail@gmail.com>
2017-11-02 20:17:43 +0200

    Update mechanism to the Identity contract

---
+++ contracts/identity/Identity.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.15;
+
+import '../helpers/Ownable.sol';
+
+contract Identity is Ownable {
+    bytes32 public identityName;
+
+    event SendEther(uint _amount, address indexed _to);
+
+    function Identity(bytes32 _identityName) {
+        identityName = _identityName;
+    }
+
+    function sendEther(uint _amount, address _to) onlyOwner returns(bool) {
+        _to.transfer(_amount);
+        SendEther(_amount, _to);
+        return true;
+    }
+}
