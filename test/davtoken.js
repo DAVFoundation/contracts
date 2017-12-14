d9e8ac43cad795a69119e5fa2287ab1029fbaa7d
Tal Ater <tal@talater.com>
2017-12-14 16:33:13 +0200

    Implemented basic DAVToken test

--- test/davtoken.js
+++ test/davtoken.js
@@ -1,28 +1,11 @@
-// Global settings and variables
-const helpers = require('./helpers');
-
 const DAVToken = artifacts.require('./DAVToken.sol');
 
-let accounts;
+contract('DAVToken', function(/*accounts*/) {
 
-// Setup procedure for the tests
-const setupDAVToken = async function() {
-  accounts = web3.eth.accounts;
-  davToken = await DAVToken.new();
-
-  return davToken;
-}
-
-// Tests
-contract('DAVToken', function(accounts) {
-
-  before(function() {
-    // helpers.etherForEveryone();
+  it('should return the correct totalSupply after construction', async function() {
+    let token = await DAVToken.new();
+    let totalSupply = await token.totalSupply();
+    assert.equal(totalSupply, (1000000 * 10**18));
   });
 
-  it('what do we test?', async function() {
-    davToken = await setupDAVToken();
-
-
-  });
 });
