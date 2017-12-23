2bbfe02e8011fe9114558463c551e0a8c9552902
Tal Ater <tal@talater.com>
2017-12-24 00:14:59 +0200

    Fixed indentation in 3 contracts

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -4,68 +4,68 @@ import './helpers/UUIDProvider.sol';
 import './DAVToken.sol';
 
 contract Identity {
-    address private tokenAddress;
+  address private tokenAddress;
 
-    mapping (bytes16 => address) public identities; // identities wallet addresses
-    mapping (bytes16 => address[]) public identityToMissions; // missions hisotry
+  mapping (bytes16 => address) public identities; // identities wallet addresses
+  mapping (bytes16 => address[]) public identityToMissions; // missions hisotry
 
-    modifier onlyOwner(bytes16 _identity) {
-        require(msg.sender == identities[_identity]);
-        _;
-    }
+  modifier onlyOwner(bytes16 _identity) {
+    require(msg.sender == identities[_identity]);
+    _;
+  }
 
-    function Identity(address _tokenAddress)  public {
-        tokenAddress = _tokenAddress;
-    }
+  function Identity(address _tokenAddress)  public {
+    tokenAddress = _tokenAddress;
+  }
 
-    /**
-     * @dev get token balance of identity
-     * @param _identity The identity bytes16 identifier
-     * @return uint which contains identity balance
-     */
-    function getBalance(bytes16 _identity) constant returns(uint) {
-        DAVToken token = DAVToken(tokenAddress);
-        return token.balanceOf(identities[_identity]);
-    }
+  /**
+   * @dev get token balance of identity
+   * @param _identity The identity bytes16 identifier
+   * @return uint which contains identity balance
+   */
+  function getBalance(bytes16 _identity) constant returns(uint) {
+    DAVToken token = DAVToken(tokenAddress);
+    return token.balanceOf(identities[_identity]);
+  }
 
-    /**
-     * @dev get identity missions history
-     * @param _identity The identity bytes16 identifier
-     * @return array of mission addresses
-     */
-    function getMissionHistory(bytes16 _identity) constant returns(address[]) {
-        return identityToMissions[_identity];
-    }
+  /**
+   * @dev get identity missions history
+   * @param _identity The identity bytes16 identifier
+   * @return array of mission addresses
+   */
+  function getMissionHistory(bytes16 _identity) constant returns(address[]) {
+    return identityToMissions[_identity];
+  }
 
-    /**
-     * @dev get wallet of identity
-     * @param _identity The identity bytes16 identifier
-     * @return address of the identity wallet
-     */
-    function getWallet(bytes16 _identity) returns(address) {
-        return identities[_identity];
-    }
+  /**
+   * @dev get wallet of identity
+   * @param _identity The identity bytes16 identifier
+   * @return address of the identity wallet
+   */
+  function getWallet(bytes16 _identity) returns(address) {
+    return identities[_identity];
+  }
 
-    /**
-     * @dev change wallet of identity
-     * @param _identity The identity bytes16 identifier
-     * @param _newWallet The address of the new wallet
-     * @return bool which indicates the operation result
-     */
-    function changeWallet(bytes16 _identity, address _newWallet) onlyOwner(_identity) returns(bool) {
-        identities[_identity] = _newWallet;
-        return true;
-    }
+  /**
+   * @dev change wallet of identity
+   * @param _identity The identity bytes16 identifier
+   * @param _newWallet The address of the new wallet
+   * @return bool which indicates the operation result
+   */
+  function changeWallet(bytes16 _identity, address _newWallet) onlyOwner(_identity) returns(bool) {
+    identities[_identity] = _newWallet;
+    return true;
+  }
 
-    /**
-     * @dev create new identity
-     * @return bool which indicates the operation result
-     */
-    function create(address _wallet) returns(bool) {
-        UUIDProvider uuidProvider = new UUIDProvider();
-        // UUIDProvider uuidProvider = UUIDProvider(0xbb17fcd3f0be84478c4772cdb1035089aa36d4d1);
-        bytes16 identityIdentifier = uuidProvider.UUID4();
-        identities[identityIdentifier] = msg.sender;
-        return true;
-    }
+  /**
+   * @dev create new identity
+   * @return bool which indicates the operation result
+   */
+  function create(address _wallet) returns(bool) {
+    UUIDProvider uuidProvider = new UUIDProvider();
+    // UUIDProvider uuidProvider = UUIDProvider(0xbb17fcd3f0be84478c4772cdb1035089aa36d4d1);
+    bytes16 identityIdentifier = uuidProvider.UUID4();
+    identities[identityIdentifier] = msg.sender;
+    return true;
+  }
 }
--- contracts/Payment.sol
+++ contracts/Payment.sol
@@ -7,133 +7,133 @@ import 'zeppelin-solidity/contracts/math/Math.sol';
 import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 
 contract Payment is Ownable {
-    address private tokenAddress;
-    address private identityAddress;
-    address private subsidyWallet;
+  address private tokenAddress;
+  address private identityAddress;
+  address private subsidyWallet;
 
-    struct Subsidy {
-        uint256 remainingSubsidyAllotment;
-        uint256 maxSubsidizedPercent;
-    }
+  struct Subsidy {
+    uint256 remainingSubsidyAllotment;
+    uint256 maxSubsidizedPercent;
+  }
 
-    mapping (bytes16 => uint256) public subsidyBalances;
-    mapping (bytes16 => Subsidy) public whiteListedIdentities;
+  mapping (bytes16 => uint256) public subsidyBalances;
+  mapping (bytes16 => Subsidy) public whiteListedIdentities;
 
-    /**
-     * @dev The constructor takes DAV token address, identity contract address and subsidy wallet address and
-     * initiate the Payment contract
-     */
-    function Payment(address _tokenAddress, address _identityAddress, address _subsidyWallet)  public {
-        tokenAddress = _tokenAddress;
-        identityAddress = _identityAddress;
-        subsidyWallet = _subsidyWallet;
-    }
+  /**
+   * @dev The constructor takes DAV token address, identity contract address and subsidy wallet address and
+   * initiate the Payment contract
+   */
+  function Payment(address _tokenAddress, address _identityAddress, address _subsidyWallet)  public {
+    tokenAddress = _tokenAddress;
+    identityAddress = _identityAddress;
+    subsidyWallet = _subsidyWallet;
+  }
 
-    /**
-     * @dev get token balance of identity
-     * @param _identity The identity bytes16 identifier
-     * @return uint which contains identity balance
-     */
-    function getBalance(bytes16 _identity) constant returns(uint) {
-        DAVToken token = DAVToken(tokenAddress);
-        Identity identity = Identity(identityAddress);
-        return token.balanceOf(identity.getWallet(_identity));
-    }
+  /**
+   * @dev get token balance of identity
+   * @param _identity The identity bytes16 identifier
+   * @return uint which contains identity balance
+   */
+  function getBalance(bytes16 _identity) constant returns(uint) {
+    DAVToken token = DAVToken(tokenAddress);
+    Identity identity = Identity(identityAddress);
+    return token.balanceOf(identity.getWallet(_identity));
+  }
 
-    /**
-     * @dev get token balance of identity
-     * @param _identityFrom The identity bytes16 identifier of the sender
-     * @param _identityTo The identity bytes16 identifier of thr receiver
-     * @param _value The tokens amount
-     * @return bool which indicates the operation result
-     */
-    function transferFrom(bytes16 _identityFrom, bytes16 _identityTo, uint256 _value) returns(bool) {
-        require(getBalance(_identityFrom) +
-                Math.min256(
-                    whiteListedIdentities[_identityTo].remainingSubsidyAllotment,
-                    SafeMath.mul(subsidyBalances[_identityFrom], whiteListedIdentities[_identityTo].maxSubsidizedPercent)
-                    )
-                >= _value);
+  /**
+   * @dev get token balance of identity
+   * @param _identityFrom The identity bytes16 identifier of the sender
+   * @param _identityTo The identity bytes16 identifier of thr receiver
+   * @param _value The tokens amount
+   * @return bool which indicates the operation result
+   */
+  function transferFrom(bytes16 _identityFrom, bytes16 _identityTo, uint256 _value) returns(bool) {
+    require(getBalance(_identityFrom) +
+        Math.min256(
+          whiteListedIdentities[_identityTo].remainingSubsidyAllotment,
+          SafeMath.mul(subsidyBalances[_identityFrom], whiteListedIdentities[_identityTo].maxSubsidizedPercent)
+          )
+        >= _value);
 
-        DAVToken token = DAVToken(tokenAddress);
-        Identity identity = Identity(identityAddress);
+    DAVToken token = DAVToken(tokenAddress);
+    Identity identity = Identity(identityAddress);
 
-        // Figure out how much of the payment will come from subsidy
-        uint256 fromSubsidy = Math.min256(
-            Math.min256(
-                    SafeMath.mul(_value, whiteListedIdentities[_identityTo].maxSubsidizedPercent),
-                    whiteListedIdentities[_identityTo].remainingSubsidyAllotment
-                ),
-            subsidyBalances[_identityFrom]
-            );
+    // Figure out how much of the payment will come from subsidy
+    uint256 fromSubsidy = Math.min256(
+      Math.min256(
+          SafeMath.mul(_value, whiteListedIdentities[_identityTo].maxSubsidizedPercent),
+          whiteListedIdentities[_identityTo].remainingSubsidyAllotment
+        ),
+      subsidyBalances[_identityFrom]
+      );
 
-        // Transfer the tokens from the wallet holding the subsidies
-        token.transferFrom(subsidyWallet, identity.getWallet(_identityTo), fromSubsidy);
+    // Transfer the tokens from the wallet holding the subsidies
+    token.transferFrom(subsidyWallet, identity.getWallet(_identityTo), fromSubsidy);
 
-        // Deduct amount that comes out of subsidy from the sender's subsidy balance
-        subsidyBalances[_identityFrom] = SafeMath.sub(subsidyBalances[_identityFrom], fromSubsidy);
+    // Deduct amount that comes out of subsidy from the sender's subsidy balance
+    subsidyBalances[_identityFrom] = SafeMath.sub(subsidyBalances[_identityFrom], fromSubsidy);
 
-        // Deduct amount that comes out of subsidy from the recipient's remaining
-        // subsidy allotment
-        whiteListedIdentities[_identityTo].remainingSubsidyAllotment = SafeMath.sub(whiteListedIdentities[_identityTo].remainingSubsidyAllotment, fromSubsidy);
+    // Deduct amount that comes out of subsidy from the recipient's remaining
+    // subsidy allotment
+    whiteListedIdentities[_identityTo].remainingSubsidyAllotment = SafeMath.sub(whiteListedIdentities[_identityTo].remainingSubsidyAllotment, fromSubsidy);
 
-        // Transfer the tokens from the wallet holding the subsidies
-        token.transferFrom(identity.getWallet(_identityFrom), identity.getWallet(_identityTo), SafeMath.sub(_value, fromSubsidy));
+    // Transfer the tokens from the wallet holding the subsidies
+    token.transferFrom(identity.getWallet(_identityFrom), identity.getWallet(_identityTo), SafeMath.sub(_value, fromSubsidy));
 
-        return true;
-    }
+    return true;
+  }
 
-    /**
-     * @dev creates subsidy to an identity
-     * @param _identity The identity bytes16 identifier
-     * @param _maxSubsidyAllotment The subsidy tokens allotment
-     * @param _maxSubsidizedPercent Transaction cost coverage percentage
-     * @return bool which indicates the operation result
-     */
-    function createSubsidy(bytes16 _identity, uint256 _maxSubsidyAllotment, uint256 _maxSubsidizedPercent) onlyOwner returns(bool) {
-        whiteListedIdentities[_identity] = Subsidy(_maxSubsidyAllotment, _maxSubsidizedPercent);
-        return true;
-    }
+  /**
+   * @dev creates subsidy to an identity
+   * @param _identity The identity bytes16 identifier
+   * @param _maxSubsidyAllotment The subsidy tokens allotment
+   * @param _maxSubsidizedPercent Transaction cost coverage percentage
+   * @return bool which indicates the operation result
+   */
+  function createSubsidy(bytes16 _identity, uint256 _maxSubsidyAllotment, uint256 _maxSubsidizedPercent) onlyOwner returns(bool) {
+    whiteListedIdentities[_identity] = Subsidy(_maxSubsidyAllotment, _maxSubsidizedPercent);
+    return true;
+  }
 
-    /**
-     * @dev deletes subsidy to an identity
-     * @param _identity The identity bytes16 identifier
-     * @return bool which indicates the operation result
-     */
-    function removeSubsidy(bytes16 _identity) onlyOwner returns(bool) {
-        delete whiteListedIdentities[_identity];
-        return true;
-    }
+  /**
+   * @dev deletes subsidy to an identity
+   * @param _identity The identity bytes16 identifier
+   * @return bool which indicates the operation result
+   */
+  function removeSubsidy(bytes16 _identity) onlyOwner returns(bool) {
+    delete whiteListedIdentities[_identity];
+    return true;
+  }
 
-    /**
-     * @dev add subsidy to a specific identity
-     * @param _identity The identity bytes16 identifier
-     * @param _value The tokens amount
-     * @return bool which indicates the operation result
-     */
-    function addSubsidyTokens(bytes16 _identity, uint256 _value) onlyOwner returns(bool) {
-        subsidyBalances[_identity] = SafeMath.add(subsidyBalances[_identity], _value);
-        return true;
-    }
+  /**
+   * @dev add subsidy to a specific identity
+   * @param _identity The identity bytes16 identifier
+   * @param _value The tokens amount
+   * @return bool which indicates the operation result
+   */
+  function addSubsidyTokens(bytes16 _identity, uint256 _value) onlyOwner returns(bool) {
+    subsidyBalances[_identity] = SafeMath.add(subsidyBalances[_identity], _value);
+    return true;
+  }
 
-    /**
-     * @dev adds subsidy to a specific identity
-     * @param _identity The identity bytes16 identifier
-     * @return bool which indicates the operation result
-     */
-    function removeSubsidyTokens(bytes16 _identity) onlyOwner returns(bool) {
-        delete subsidyBalances[_identity];
-        return true;
-    }
+  /**
+   * @dev adds subsidy to a specific identity
+   * @param _identity The identity bytes16 identifier
+   * @return bool which indicates the operation result
+   */
+  function removeSubsidyTokens(bytes16 _identity) onlyOwner returns(bool) {
+    delete subsidyBalances[_identity];
+    return true;
+  }
 
-    /**
-     * @dev sets new subsidy wallet
-     * @param _subsidyWallet Address of the new subsidy wallet
-     * @return bool which indicates the operation result
-     */
-    function setSubsidyWalletAddress(address _subsidyWallet) onlyOwner returns(bool) {
-        subsidyWallet = _subsidyWallet;
-        return true;
-    }
+  /**
+   * @dev sets new subsidy wallet
+   * @param _subsidyWallet Address of the new subsidy wallet
+   * @return bool which indicates the operation result
+   */
+  function setSubsidyWalletAddress(address _subsidyWallet) onlyOwner returns(bool) {
+    subsidyWallet = _subsidyWallet;
+    return true;
+  }
 
 }
