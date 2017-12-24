9c8fca4b29ed3733faf5eec6b95c92577643c858
Tal Ater <tal@talater.com>
2017-12-24 11:33:14 +0200

    Removed old contracts from migration file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,5 @@
 var DAVToken = artifacts.require('./DAVToken.sol');
-var Identity = artifacts.require('./Identity.sol');
-var Payment = artifacts.require('./Payment.sol');
 
 module.exports = function(deployer) {
   deployer.deploy(DAVToken);
-  deployer.deploy(Identity);
-  deployer.deploy(Payment);
 };
