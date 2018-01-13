0f6f0ee41bd739f518c030c5c56d70f427712465
Tal Ater <tal@talater.com>
2018-01-13 21:57:21 +0200

    Added Identity contract with first function

---
+++ contracts/Identity.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.15;
+
+
+/**
+ * @title DAV Token
+ * @dev ERC20 token
+ */
+contract Identity {
+
+  function parseAddr(string _address) internal pure returns (address){
+    bytes memory a = bytes(_address);
+    uint160 iaddr = 0;
+    uint160 b1;
+    uint160 b2;
+    for (uint i=2; i<2+2*20; i+=2){
+      iaddr *= 256;
+      b1 = uint160(a[i]);
+      b2 = uint160(a[i+1]);
+      if ((b1 >= 97)&&(b1 <= 102)) b1 -= 87;
+      else if ((b1 >= 48)&&(b1 <= 57)) b1 -= 48;
+      if ((b2 >= 97)&&(b2 <= 102)) b2 -= 87;
+      else if ((b2 >= 48)&&(b2 <= 57)) b2 -= 48;
+      iaddr += (b1*16+b2);
+    }
+    return address(iaddr);
+  }
+
+  function create(string _public_key, uint8 v, bytes32 r, bytes32 s) public pure returns (bool) {
+    require(
+      ecrecover(keccak256(_public_key), v, r, s) == parseAddr(_public_key)
+    );
+    return true;
+  }
+}
---
+++ test/Identity.js
@@ -0,0 +1,27 @@
+const Identity = artifacts.require('./Identity.sol');
+const expectThrow = require('./helpers/expectThrow');
+
+const sampleIdentity = {
+  address: '0x17325a469aef3472aa58dfdcf672881d79b31d58',
+  v: 28,
+  r: '0x092851aac67ddb02c0bd976142f66c937d920fee4dbb305890452b67abb1b9b8',
+  s: '0x12f6f410abbb099561c5018f4fc5d9ad7a4c5c2a6f01c594f66a3a243bc7f713',
+  invalidR: '0x092851aac67ddb02c0bd976142f66c937d920fee4dbb305890452b67abb1b9b0',
+};
+
+
+contract('Identity', function() {
+
+  it('should not throw when attempting to create with a valid signature', async function() {
+    const IdentityContract = await Identity.new();
+    IdentityContract.create(sampleIdentity.address, sampleIdentity.v, sampleIdentity.r, sampleIdentity.s);
+  });
+
+  it('should throw when attempting to create with an invalid signature', async function() {
+    const IdentityContract = await Identity.new();
+    await expectThrow(
+      IdentityContract.create(sampleIdentity.address, sampleIdentity.v, sampleIdentity.invalidR, sampleIdentity.s)
+    );
+  });
+
+});
