839818f2ed2ce7400e2651e700c8cf986bd50968
Tal Ater <tal@talater.com>
2018-02-28 13:43:21 +0200

    Added BasicMission to deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 var DAVToken = artifacts.require('./DAVToken.sol');
 var Identity = artifacts.require('./Identity.sol');
+var BasicMission = artifacts.require('./BasicMission.sol');
 
 module.exports = async (deployer) => {
   await deployer.deploy(DAVToken);
   deployer.deploy(Identity, DAVToken.address);
+  deployer.deploy(BasicMission);
 };
