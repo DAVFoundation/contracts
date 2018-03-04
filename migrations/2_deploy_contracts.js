e1c6711ae83bc112e4d9c2491343f6cb0d1ab27a
Tal Ater <tal@talater.com>
2018-03-04 13:27:28 +0200

    Removed BasicMission and Identity

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,5 @@
 var DAVToken = artifacts.require('./DAVToken.sol');
-var Identity = artifacts.require('./Identity.sol');
-var BasicMission = artifacts.require('./BasicMission.sol');
 
 module.exports = async (deployer) => {
   await deployer.deploy(DAVToken);
-  await deployer.deploy(Identity, DAVToken.address);
-  deployer.deploy(BasicMission, Identity.address, DAVToken.address);
 };
