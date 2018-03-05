345a1ae201d46c0b4a7770b1b27a1191fe1a8fe5
Tal Ater <tal@talater.com>
2018-03-05 18:41:29 +0200

    Simplified DAVToken test with a beforeEach to deploy the contract

--- test/contracts/DAVToken.js
+++ test/contracts/DAVToken.js
@@ -3,9 +3,14 @@ const expectThrow = require('../helpers/expectThrow');
 const totalSupplySetting = 100000;
 
 contract('DAVToken', function(accounts) {
+  let token;
+
+  beforeEach(async () => {
+    token = await DAVToken.new();
+  });
+
   describe('totalSupply()', () => {
     it('should return the correct total supply after construction', async function() {
-      let token = await DAVToken.new();
       let totalSupply = await token.totalSupply();
       assert.equal(totalSupply, totalSupplySetting);
     });
@@ -13,7 +18,6 @@ contract('DAVToken', function(accounts) {
 
   describe('balanceOf()', () => {
     it('should return the correct balance of an account', async function() {
-      let token = await DAVToken.new();
       let firstAccountBalance = await token.balanceOf(accounts[0]);
       assert.equal(firstAccountBalance, totalSupplySetting);
     });
@@ -21,7 +25,6 @@ contract('DAVToken', function(accounts) {
 
   describe('transfer()', () => {
     it('should transfer from sender when calling transfer()', async function() {
-      let token = await DAVToken.new();
       await token.transfer(accounts[1], totalSupplySetting);
       let firstAccountBalance = await token.balanceOf(accounts[0]);
       let secondAccountBalance = await token.balanceOf(accounts[1]);
@@ -30,19 +33,16 @@ contract('DAVToken', function(accounts) {
     });
 
     it('should throw an error when trying to transfer more than balance', async function() {
-      let token = await DAVToken.new();
       await expectThrow(token.transfer(accounts[1], totalSupplySetting + 1));
     });
 
     it('should throw an error when trying to transfer without approval', async function() {
-      let token = await DAVToken.new();
       await expectThrow(token.transferFrom(accounts[1], accounts[0], 1));
     });
   });
 
   describe('approve()', () => {
     it('should allow to transfer with transferFrom after approval but no more than approved amount', async function() {
-      let token = await DAVToken.new();
       await token.approve(accounts[0], 2);
       await token.transferFrom(accounts[0], accounts[1], 1);
       await token.transferFrom(accounts[0], accounts[1], 1);
@@ -52,7 +52,6 @@ contract('DAVToken', function(accounts) {
 
   describe('allowance()', () => {
     it('should return the correct allowance of an account to another', async function() {
-      let token = await DAVToken.new();
       assert.equal(await token.allowance(accounts[0], accounts[0]), 0);
       await token.approve(accounts[0], 1);
       assert.equal(await token.allowance(accounts[0], accounts[0]), 1);
@@ -63,21 +62,18 @@ contract('DAVToken', function(accounts) {
 
   describe('pause()', () => {
     it('should be callable by owner and modify pause state', async function() {
-      let token = await DAVToken.new();
       assert.equal(await token.paused(), false);
       await token.pause();
       assert.equal(await token.paused(), true);
     });
 
     it('should throw an error when trying to pause while paused', async function() {
-      let token = await DAVToken.new();
       await token.pause();
       await expectThrow(token.pause());
       assert.equal(await token.paused(), true);
     });
 
     it('should throw errors when calling transfer, transferFrom, approve, increaseApproval, or decreaseApproval while paused', async function() {
-      let token = await DAVToken.new();
       await token.approve(accounts[0], 2);
       await token.pause();
       await expectThrow(token.transferFrom(accounts[0], accounts[1], 1));
@@ -88,7 +84,6 @@ contract('DAVToken', function(accounts) {
     });
 
     it('should throw an error if a non-owner tries to pause', async () => {
-      let token = await DAVToken.new();
       await expectThrow(token.pause({ from: accounts[1] }));
       assert.equal(await token.paused(), false);
     });
@@ -96,7 +91,6 @@ contract('DAVToken', function(accounts) {
 
   describe('unpause()', () => {
     it('should be callable by owner and modify state', async function() {
-      let token = await DAVToken.new();
       assert.equal(await token.paused(), false);
       await token.pause();
       assert.equal(await token.paused(), true);
@@ -105,13 +99,11 @@ contract('DAVToken', function(accounts) {
     });
 
     it('should throw an error when trying to unpause while not paused', async function() {
-      let token = await DAVToken.new();
       await expectThrow(token.unpause());
       assert.equal(await token.paused(), false);
     });
 
     it('should throw an error if a non-owner tries to unpause', async () => {
-      let token = await DAVToken.new();
       await token.pause();
       await expectThrow(token.unpause({ from: accounts[1] }));
       assert.equal(await token.paused(), true);
@@ -120,7 +112,6 @@ contract('DAVToken', function(accounts) {
 
   describe('increaseApproval()', () => {
     it('should allow increasing the allowance with increaseApproval', async function() {
-      let token = await DAVToken.new();
       await token.increaseApproval(accounts[0], 1);
       assert.equal(await token.allowance(accounts[0], accounts[0]), 1);
     });
@@ -128,14 +119,12 @@ contract('DAVToken', function(accounts) {
 
   describe('decreaseApproval()', () => {
     it('should allow decreasing the allowance with decreaseApproval', async function() {
-      let token = await DAVToken.new();
       await token.approve(accounts[0], 5);
       await token.decreaseApproval(accounts[0], 1);
       assert.equal(await token.allowance(accounts[0], accounts[0]), 4);
     });
 
     it('should set approved amount to 0 if trying to decreaseApproval to below 0', async function() {
-      let token = await DAVToken.new();
       await token.approve(accounts[0], 1);
       await token.decreaseApproval(accounts[0], 2);
       assert.equal(await token.allowance(accounts[0], accounts[0]), 0);
