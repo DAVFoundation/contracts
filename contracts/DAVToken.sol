3ac2f56c47c6177ae54616df3fef6d1b82dbc024
Tal Ater <tal@talater.com>
2018-03-05 19:12:40 +0200

    DAVToken is no longer Pausable but a PausableToken
    Simplifies implementation significantly

--- contracts/DAVToken.sol
+++ contracts/DAVToken.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.18;
 import './interfaces/IDAVToken.sol';
 import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
 import 'zeppelin-solidity/contracts/token/ERC20/BurnableToken.sol';
+import 'zeppelin-solidity/contracts/token/ERC20/PausableToken.sol';
 import 'zeppelin-solidity/contracts/lifecycle/Pausable.sol';
 
 
@@ -10,7 +11,7 @@ import 'zeppelin-solidity/contracts/lifecycle/Pausable.sol';
  * @title DAV Token
  * @dev ERC20 token
  */
-contract DAVToken is IDAVToken, MintableToken, BurnableToken, Pausable {
+contract DAVToken is IDAVToken, MintableToken, BurnableToken, PausableToken {
 
   // Token constants
   // NOTE: These are placeholder values used during development. Final inital supply will be determined and announced later
@@ -28,64 +29,4 @@ contract DAVToken is IDAVToken, MintableToken, BurnableToken, Pausable {
     balances[msg.sender] = initialSupply;
   }
 
-  /**
-  * @notice transfer token for a specified address
-  * @param _to The address to transfer to.
-  * @param _value The amount to be transferred.
-  */
-  function transfer(address _to, uint256 _value) public whenNotPaused returns (bool) {
-    return super.transfer(_to, _value);
-  }
-
-  /**
-   * @notice Transfer tokens from one address to another
-   * @param _from address The address which you want to send tokens from
-   * @param _to address The address which you want to transfer to
-   * @param _value uint256 the amount of tokens to be transferred
-   */
-  function transferFrom(address _from, address _to, uint256 _value) public whenNotPaused returns (bool) {
-    return super.transferFrom(_from, _to, _value);
-  }
-
-  /**
-   * @notice Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
-   *
-   * Beware that changing an allowance with this method brings the risk that someone may use both the old
-   * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
-   * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
-   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-   * @param _spender The address which will spend the funds.
-   * @param _value The amount of tokens to be spent.
-   */
-  function approve(address _spender, uint256 _value) public whenNotPaused returns (bool) {
-    return super.approve(_spender, _value);
-  }
-
-  /**
-   * @notice Increase the amount of tokens that an owner is allowed to a spender.
-   *
-   * approve should be called when allowed[_spender] == 0. To increment
-   * allowed value is better to use this function to avoid 2 calls (and wait until
-   * the first transaction is mined)
-   * From MonolithDAO Token.sol
-   * @param _spender The address which will spend the funds.
-   * @param _addedValue The amount of tokens to increase the allowance by.
-   */
-  function increaseApproval(address _spender, uint _addedValue) public whenNotPaused returns (bool success) {
-    return super.increaseApproval(_spender, _addedValue);
-  }
-
-  /**
-   * @notice Decrease the amount of tokens that an owner allowed to a spender.
-   *
-   * approve should be called when allowed[_spender] == 0. To decrement
-   * allowed value is better to use this function to avoid 2 calls (and wait until
-   * the first transaction is mined)
-   * From MonolithDAO Token.sol
-   * @param _spender The address which will spend the funds.
-   * @param _subtractedValue The amount of tokens to decrease the allowance by.
-   */
-  function decreaseApproval(address _spender, uint _subtractedValue) public whenNotPaused returns (bool success) {
-    return super.decreaseApproval(_spender, _subtractedValue);
-  }
 }
