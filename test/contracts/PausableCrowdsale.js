9aa735bc3d4dedb6221e77c503b6c55e0393a0be
Tal Ater <tal@talater.com>
2018-03-22 16:41:34 +0200

    Made transaction origin more elegant in PausableCrowdsale tests

--- test/contracts/PausableCrowdsale.js
+++ test/contracts/PausableCrowdsale.js
@@ -21,17 +21,18 @@ contract('DAVCrowdsale is PausableCrowdsale', ([owner, bank, buyer]) => {
 
   describe('pause()', () => {
     describe('when the sender is the crowdsale owner', () => {
+      const from = owner;
 
       describe('when the crowdsale is unpaused', () => {
 
         it('pauses the crowdsale', async () => {
-          await crowdsale.pause({ from: owner });
+          await crowdsale.pause({ from });
           const paused = await crowdsale.paused();
           assert.equal(paused, true);
         });
 
         it('emits a Pause event', async () => {
-          const { logs } = await crowdsale.pause({ from: owner });
+          const { logs } = await crowdsale.pause({ from });
           assert.equal(logs.length, 1);
           assert.equal(logs[0].event, 'Pause');
         });
@@ -39,20 +40,21 @@ contract('DAVCrowdsale is PausableCrowdsale', ([owner, bank, buyer]) => {
 
       describe('when the crowdsale is paused', () => {
         beforeEach(async () => {
-          await crowdsale.pause({ from: owner });
+          await crowdsale.pause({ from });
         });
 
         it('reverts', async () => {
-          await assertRevert(crowdsale.pause({ from: owner }));
+          await assertRevert(crowdsale.pause({ from }));
         });
       });
 
     });
 
     describe('when the sender is not the crowdsale owner', () => {
+      const from = bank;
 
       it('reverts', async () => {
-        await assertRevert(crowdsale.pause({ from: bank }));
+        await assertRevert(crowdsale.pause({ from }));
       });
 
     });
@@ -60,20 +62,21 @@ contract('DAVCrowdsale is PausableCrowdsale', ([owner, bank, buyer]) => {
 
   describe('unpause()', () => {
     describe('when the sender is the crowdsale owner', () => {
+      const from = owner;
 
       describe('when the crowdsale is paused', () => {
         beforeEach(async () => {
-          await crowdsale.pause({ from: owner });
+          await crowdsale.pause({ from });
         });
 
         it('unpauses the crowdsale', async () => {
-          await crowdsale.unpause({ from: owner });
+          await crowdsale.unpause({ from });
           const paused = await crowdsale.paused();
           assert.equal(paused, false);
         });
 
         it('emits an Unpause event', async () => {
-          const { logs } = await crowdsale.unpause({ from: owner });
+          const { logs } = await crowdsale.unpause({ from });
           assert.equal(logs.length, 1);
           assert.equal(logs[0].event, 'Unpause');
         });
@@ -81,13 +84,15 @@ contract('DAVCrowdsale is PausableCrowdsale', ([owner, bank, buyer]) => {
 
       describe('when the crowdsale is unpaused', () => {
         it('reverts', async () => {
-          await assertRevert(crowdsale.unpause({ from: owner }));
+          await assertRevert(crowdsale.unpause({ from }));
         });
       });
 
     });
 
     describe('when the sender is not the crowdsale owner', () => {
+      const from = bank;
+
       it('reverts', async () => {
         await assertRevert(crowdsale.unpause({ from: bank }));
       });
@@ -95,28 +100,30 @@ contract('DAVCrowdsale is PausableCrowdsale', ([owner, bank, buyer]) => {
   });
 
   describe('paused', () => {
+    const from = owner;
 
     it('is not paused by default', async () => {
-      const paused = await crowdsale.paused({ from: owner });
+      const paused = await crowdsale.paused({ from });
       assert.equal(paused, false);
     });
 
     it('is paused after being paused', async () => {
-      await crowdsale.pause({ from: owner });
-      const paused = await crowdsale.paused({ from: owner });
+      await crowdsale.pause({ from });
+      const paused = await crowdsale.paused({ from });
       assert.equal(paused, true);
     });
 
     it('is not paused after being paused and then unpaused', async () => {
-      await crowdsale.pause({ from: owner });
-      await crowdsale.unpause({ from: owner });
-      const paused = await crowdsale.paused({ from: owner });
+      await crowdsale.pause({ from });
+      await crowdsale.unpause({ from });
+      const paused = await crowdsale.paused({ from });
       assert.equal(paused, false);
     });
 
   });
 
   describe('when the crowdsale is paused', () => {
+    const from = buyer;
 
     beforeEach(async () => {
       await crowdsale.pause({ from: owner });
@@ -124,13 +131,13 @@ contract('DAVCrowdsale is PausableCrowdsale', ([owner, bank, buyer]) => {
 
     describe('high-level purchase using fallback function', () => {
       it('reverts', async () => {
-        await assertRevert(crowdsale.sendTransaction({ from: buyer, value }));
+        await assertRevert(crowdsale.sendTransaction({ from, value }));
       });
     });
 
     describe('buyTokens()', () => {
       it('reverts', async () => {
-        await assertRevert(crowdsale.buyTokens(buyer, { from: buyer, value }));
+        await assertRevert(crowdsale.buyTokens(buyer, { from, value }));
       });
     });
 
