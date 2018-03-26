443793fb67296ed21de72034c5f3d4d3e9d38593
Tal Ater <tal@talater.com>
2018-03-26 20:20:03 +0300

    Got rid of expectThrow

--- test/contracts/DAVToken.js
+++ test/contracts/DAVToken.js
@@ -1,5 +1,5 @@
 const DAVToken = artifacts.require('./mocks/DAVToken.sol');
-const expectThrow = require('../helpers/expectThrow');
+const assertRevert = require('../helpers/assertRevert');
 const totalSupply = 100000;
 
 contract('DAVToken', function([user1, user2]) {
@@ -33,11 +33,11 @@ contract('DAVToken', function([user1, user2]) {
     });
 
     it('should revert when trying to transfer more than balance', async function() {
-      await expectThrow(token.transfer(user2, totalSupply + 1));
+      await assertRevert(token.transfer(user2, totalSupply + 1));
     });
 
     it('should revert when trying to transfer without approval', async function() {
-      await expectThrow(token.transferFrom(user2, user1, 1));
+      await assertRevert(token.transferFrom(user2, user1, 1));
     });
   });
 
@@ -46,7 +46,7 @@ contract('DAVToken', function([user1, user2]) {
       await token.approve(user1, 2);
       await token.transferFrom(user1, user2, 1);
       await token.transferFrom(user1, user2, 1);
-      await expectThrow(token.transferFrom(user1, user2, 1));
+      await assertRevert(token.transferFrom(user1, user2, 1));
     });
   });
 
@@ -69,22 +69,22 @@ contract('DAVToken', function([user1, user2]) {
 
     it('should revert when trying to pause while paused', async function() {
       await token.pause();
-      await expectThrow(token.pause());
+      await assertRevert(token.pause());
       assert.equal(await token.paused(), true);
     });
 
     it('should revert when calling transfer, transferFrom, approve, increaseApproval, or decreaseApproval while paused', async function() {
       await token.approve(user1, 2);
       await token.pause();
-      await expectThrow(token.transferFrom(user1, user2, 1));
-      await expectThrow(token.transfer(user2, 1));
-      await expectThrow(token.approve(user1, 2));
-      await expectThrow(token.increaseApproval(user1, 1));
-      await expectThrow(token.decreaseApproval(user1, 1));
+      await assertRevert(token.transferFrom(user1, user2, 1));
+      await assertRevert(token.transfer(user2, 1));
+      await assertRevert(token.approve(user1, 2));
+      await assertRevert(token.increaseApproval(user1, 1));
+      await assertRevert(token.decreaseApproval(user1, 1));
     });
 
     it('should revert if a non-owner tries to pause', async () => {
-      await expectThrow(token.pause({ from: user2 }));
+      await assertRevert(token.pause({ from: user2 }));
       assert.equal(await token.paused(), false);
     });
   });
@@ -99,13 +99,13 @@ contract('DAVToken', function([user1, user2]) {
     });
 
     it('should revert when trying to unpause while not paused', async function() {
-      await expectThrow(token.unpause());
+      await assertRevert(token.unpause());
       assert.equal(await token.paused(), false);
     });
 
     it('should revert if a non-owner tries to unpause', async () => {
       await token.pause();
-      await expectThrow(token.unpause({ from: user2 }));
+      await assertRevert(token.unpause({ from: user2 }));
       assert.equal(await token.paused(), true);
     });
   });
