f1d67bf9f5f9f98e29bfe344eb9d48aa8d13d7b4
Tal Ater <tal@talater.com>
2018-03-26 14:23:27 +0300

    Made migrations file clearer

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,12 @@
 const DAVToken = artifacts.require('./DAVToken.sol');
 const DAVCrowdsale = artifacts.require('./DAVCrowdsale.sol');
 
-module.exports = async (deployer, network, accounts) => {
+module.exports = async (deployer, network, [bank]) => {
   // Conversion rate between wei to DAV's smallest unit (1/1e18 DAV)
-  const _rate = 10000;
+  const rate = 10000;
+  // Minimal acceptable contribution amount
+  const minimalContribution = web3.toWei(0.2, 'ether');
 
   await deployer.deploy(DAVToken);
-  await deployer.deploy(DAVCrowdsale, _rate, accounts[0], DAVToken.address, web3.toWei(0.2, 'ether'));
+  await deployer.deploy(DAVCrowdsale, rate, bank, DAVToken.address, minimalContribution);
 };
