af31c769f7e593709830401334688805a014719c
Anatoly T <antt001@gmail.com>
2018-04-01 16:04:15 +0300

    feat: deploy to main and test nets

--- truffle.js
+++ truffle.js
@@ -1,5 +1,36 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const mnemonic = require('./mnemonic');
+
 module.exports = {
   networks: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/wUiZtmeZ1KwjFrcC8zRO');
+      },
+      network_id: 3,
+      gas: 4600000
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/wUiZtmeZ1KwjFrcC8zRO');
+      },
+      network_id: 4,
+      gas: 4600000
+    },
+    mainnet: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/wUiZtmeZ1KwjFrcC8zRO');
+      },
+      network_id: 1,
+      gas: 6712388,
+      gasPrice: 2000000000,
+    },
     development: {
       host: 'localhost',
       port: 8545,
