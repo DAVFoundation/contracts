72b17670d61b1a7c551bdd592e1e0149f0612539
Tal Ater <tal@talater.com>
2018-04-29 16:37:54 +0300

    Made total supply in deploy script a constant

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,8 @@ const DAVToken = artifacts.require('./DAVToken.sol');
 const DAVCrowdsale = artifacts.require('./DAVCrowdsale.sol');
 
 module.exports = async (deployer, network, [bank]) => {
+  // Total supply
+  const totalSupply = web3.toWei(1771428571, 'ether');
   // Conversion rate between wei to DAV's smallest unit (1/1e18 DAV)
   const rate = 10000;
   // Minimal acceptable contribution amount
@@ -10,6 +12,6 @@ module.exports = async (deployer, network, [bank]) => {
   const openingTime = Date.parse('11 June 2018 13:00:00 GMT')/1000;
   const closingTime = Date.parse('25 June 2018 13:00:00 GMT')/1000;
 
-  await deployer.deploy(DAVToken, 1000000000000000000000000);
+  await deployer.deploy(DAVToken, totalSupply);
   await deployer.deploy(DAVCrowdsale, rate, bank, DAVToken.address, minimalContribution, openingTime, closingTime);
 };
